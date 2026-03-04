1d9a272106c41c624d469a6de6f3ccbd2492296d
Taylor Schley <taylorschley@me.com>
2026-03-04 12:48:21 -0600

    chore: lint

--- config.mjs
+++ config.mjs
@@ -1,30 +1,24 @@
-import { context } from 'esbuild';
-import { BitburnerPlugin } from 'esbuild-bitburner-plugin';
+import { context } from 'esbuild'
+import { BitburnerPlugin } from 'esbuild-bitburner-plugin'
 
-const createContext = async () => await context({
-  entryPoints: [
-    'servers/**/*.js',
-    'servers/**/*.jsx',
-    'servers/**/*.ts',
-    'servers/**/*.tsx',
-  ],
-  outbase: "./servers",
-  outdir: "./build",
-  plugins: [
-    BitburnerPlugin({
-      port: 12525,
-      types: 'NetscriptDefinitions.d.ts',
-      mirror: {
-      },
-      distribute: {
-      },
-    })
-  ],
-  bundle: true,
-  format: 'esm',
-  platform: 'browser',
-  logLevel: 'debug',
-});
+const createContext = async () =>
+  await context({
+    entryPoints: ['servers/**/*.js', 'servers/**/*.jsx', 'servers/**/*.ts', 'servers/**/*.tsx'],
+    outbase: './servers',
+    outdir: './build',
+    plugins: [
+      BitburnerPlugin({
+        port: 12525,
+        types: 'NetscriptDefinitions.d.ts',
+        mirror: {},
+        distribute: {},
+      }),
+    ],
+    bundle: true,
+    format: 'esm',
+    platform: 'browser',
+    logLevel: 'debug',
+  })
 
-const ctx = await createContext();
-ctx.watch();
+const ctx = await createContext()
+ctx.watch()
