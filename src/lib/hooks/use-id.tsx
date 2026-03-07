0d92ab0dec6a7c1e584ce4079374b330ee0a587d
Taylor Schley <taylorschley@me.com>
2026-03-07 07:56:05 -0600

    feat: better use-id

--- src/lib/hooks/use-id.tsx
+++ src/lib/hooks/use-id.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-let id = 0
-
 export function useId() {
-  return React.useMemo(() => id++, [])
+  return React.useMemo(() => new TextDecoder().decode(crypto.getRandomValues(new Uint8Array(5))), [])
 }
