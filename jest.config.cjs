35fc9e406d4435a4cd671001f12d7f0081474988
Taylor Schley <taylorschley@me.com>
2026-04-06 08:56:23 -0500

    feat: get jest working again

---
+++ jest.config.cjs
@@ -0,0 +1,23 @@
+// jest.config.js
+const { createDefaultPreset, pathsToModuleNameMapper } = require('ts-jest')
+
+// Read and strip comments so JSON.parse can handle it
+const { compilerOptions } = require('./tsconfig.json')
+
+const tsJestTransformCfg = createDefaultPreset().transform
+
+/** @type {import('jest').Config} */
+const config = {
+  clearMocks: true,
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.?([mc])[jt]s?(x)', '**/*.+(spec|test).?([mc])[jt]s?(x)'],
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  roots: ['<rootDir>'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+}
+
+module.exports = config
